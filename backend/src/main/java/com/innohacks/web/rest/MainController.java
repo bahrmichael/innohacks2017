6520f06ec30a80d85f35406d746c442113723aea
bahrmichael <mbahr92@gmail.com>
2017-09-30 19:43:11 +0200

    forgot the main class :(

--- backend/src/main/java/com/innohacks/web/rest/MainController.java
+++ backend/src/main/java/com/innohacks/web/rest/MainController.java
@@ -1,5 +1,8 @@
 package com.innohacks.web.rest;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import com.innohacks.domain.Sentence;
@@ -20,6 +23,9 @@ public class MainController {
 
     private final SentenceService sentenceService;
     private final UserResultService userResultService;
+    private Map<String, Sentence> recentSentenceOfUser = new HashMap<>();
+    private Map<String, List<String>> recentUnknownWords = new HashMap<>();
+    private Map<String, Integer> recentUnknownWordCount = new HashMap<>();
 
     public MainController(final SentenceService sentenceService,
                           final UserResultService userResultService) {
@@ -39,7 +45,7 @@ public class MainController {
 
     @GetMapping("/sentence/random/")
     public String getRandomSentence() {
-        return sentenceService.getRandomSentence().getFirstLanguage();
+        return sentenceService.getRandomSentence().getEnglish();
     }
 
     @GetMapping("/sentence/{sentence}/translate/")
@@ -64,7 +70,7 @@ public class MainController {
     public ResponseEntity getResult(@PathVariable("user") String user, @PathVariable("status") UserResultStatus status) {
         Optional<Sentence> optionalSentence = sentenceService.findASentenceWithStatus(user, status);
         if (optionalSentence.isPresent()) {
-            return ResponseEntity.ok(optionalSentence.get().getSecondLanguage());
+            return ResponseEntity.ok(optionalSentence.get().getGerman());
         } else {
             return ResponseEntity.notFound().build();
         }
@@ -72,7 +78,34 @@ public class MainController {
 
     @GetMapping("/user/{user}/sentence/")
     public ResponseEntity getResult(@PathVariable("user") String user) {
-        // todo miro
-        return ResponseEntity.ok().build();
+        Sentence sentence = sentenceService.getUnknownSentenceForUser(user);
+        recentSentenceOfUser.put(user, sentence);
+        return ResponseEntity.ok(sentence.getGerman());
+    }
+
+    @GetMapping("/user/{user}/last-sentence/")
+    public ResponseEntity getLastSentence(@PathVariable("user") String user) {
+        return ResponseEntity.ok(recentSentenceOfUser.get(user));
+    }
+
+    @GetMapping("/user/{user}/last-sentence/unknown-words/")
+    public ResponseEntity getUnknownWordsForLastSentence(@PathVariable("user") String user) {
+        List<String> unknownWords = sentenceService.getUnknownWords(user, recentSentenceOfUser.get(user));
+        recentUnknownWords.put(user, unknownWords);
+        recentUnknownWordCount.put(user, 0);
+        return ResponseEntity.ok(unknownWords);
+    }
+
+    @GetMapping("/user/{user}/last-sentence/explain/current-word/")
+    public ResponseEntity explainCurrentWord(@PathVariable("user") String user) {
+        Integer index = recentUnknownWordCount.get(user);
+        recentUnknownWordCount.put(user, index + 1);
+        List<String> unknownWords = recentUnknownWords.get(user);
+        if (unknownWords.size() < index + 1) {
+            return ResponseEntity.notFound().build();
+        } else {
+            String word = unknownWords.get(index);
+            return ResponseEntity.ok(word);
+        }
     }
 }
